3194af45da03157fa7fc53ce8dfab9f783fbed0c
AlexanderYepez-code <alexsaomn17@gmail.com>
2026-06-15 15:39:55 +0200

    Backend structure

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,13 +1,1 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/authController');
-const authMiddleware = require('../middlewares/authMiddleware');
 
-// Public routes
-router.post('/register', authController.register);
-router.post('/login', authController.login);
-
-// Protected routes (require valid JWT)
-router.get('/me', authMiddleware, authController.getMe);
-
-module.exports = router;

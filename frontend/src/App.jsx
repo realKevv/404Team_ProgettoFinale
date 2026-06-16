6aea2e013809e494775ea5c2b981a3fec7af0dc5
biancaandreeacioc <biancaandreea.ciocoiu@gmail.com>
2026-06-16 21:53:47 +0200

    formLogin

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,22 @@ import './App.css'
 import { Sidebar } from './components/Sidebar'
 import Navbar from './components/Navbar'
 import HomePage from './pages/HomePage'
+import Login from './pages/Login'
 
 function App() {
 
   return (
     <>
-      <Navbar />
-      <HomePage />
-      <Sidebar />
+      {/* <Navbar />
+      <HomePage /> */}
+      <Login />
+      {/* <Sidebar /> */}
+
+
     </>
   )
+
+
 }
 
 export default App

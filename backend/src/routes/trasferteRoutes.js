a453050e51cbbd264f005b74de3b182d12efb204
AlexanderYepez-code <alexsaomn17@gmail.com>
2026-06-16 18:04:59 +0200

    prova

--- backend/src/routes/trasferteRoutes.js
+++ backend/src/routes/trasferteRoutes.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const { verifyToken } = require("../middlewares/authMiddleware");
 const {
   getAllTrasferte,
   getTrasferteById,
   createTrasferta,
 } = require("../controllers/trasferteController");
 
-router.get("/", getAllTrasferte);
-router.get("/:id", getTrasferteById);
-router.post("/", createTrasferta);
+router.get("/", verifyToken, getAllTrasferte);
+router.get("/:id", verifyToken, getTrasferteById);
+router.post("/", verifyToken, createTrasferta);
 
 module.exports = router;
